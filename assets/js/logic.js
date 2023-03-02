10e209943a143f287648f7ed0b717d2938a5541c
naufaladdhiya <naufal.addhiya@gmail>
2023-03-02 22:43:00 +0700

    First Commit

---
+++ assets/js/logic.js
@@ -0,0 +1,8 @@
+function hamburger() {
+  var i = document.getElementById('link');
+  if (i.style.display === 'block') {
+    i.style.display = 'none';
+  } else {
+    i.style.display = 'block';
+  }
+}
